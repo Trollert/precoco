ec5183341ab763d1fc0e1d86130ab5647b08d989
Tobias Blank <ga49qil@mytum.de>
2020-07-13 12:14:31 +0200

    initial commit

--- precoco/update/install_update.py
+++ precoco/update/install_update.py
@@ -1,11 +1,9 @@
 import zipfile
-import shutil
+from distutils.dir_util import copy_tree, remove_tree
 
 with zipfile.ZipFile('update.zip', 'r') as zip_ref:
     zip_ref.extractall('../..')
 
 zip_path = '../../precoco-master'
-for file in zip_path.glob('*.*'):
-    shutil.copy(zip_path, '../..')
-
-shutil.rmtree('../../precoco-master')
+copy_tree(zip_path, '../..')
+remove_tree(zip_path)
