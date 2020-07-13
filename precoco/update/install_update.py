456a1c5353dbe9b352fc8c8c1fcee7115e3d2dcb
Tobias Blank <ga49qil@mytum.de>
2020-07-13 11:00:10 +0200

    initial commit

--- precoco/update/install_update.py
+++ precoco/update/install_update.py
@@ -1,4 +1,11 @@
 import zipfile
+import shutil
 
 with zipfile.ZipFile('update.zip', 'r') as zip_ref:
     zip_ref.extractall('../..')
+
+zip_path = '../../precoco-master'
+for file in zip_path.glob('*.*'):
+    shutil.copy(zip_path, '../..')
+
+shutil.rmtree('../../precoco-master')
